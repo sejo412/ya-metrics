69ebdb91b7c8cf31a3370f5bdc933591433e407e
sejo <sejo412@gmail.com>
2024-11-24 17:18:08 +0300

    staging

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"path"
 	"reflect"
 	"runtime"
 	"time"
 )
 
 const (
+	UploadsPrefix string = "update"
 	GaugePrefix   string = "gauge"
 	CounterPrefix string = "counter"
 )
@@ -37,38 +39,86 @@ type Report struct {
 	Counter map[string]int64
 }
 
-var pollCount int64 = 0
+var (
+	pollCount   int64  = 0
+	root        string = ""
+	metricName  string = ""
+	float64Type        = reflect.TypeOf(float64(0))
+	int64Type          = reflect.TypeOf(int64(0))
+)
 
 func main() {
 	m := new(Metrics)
-	go pollMetrics(m)
-	for _ = range time.Tick(pollInterval) {
-		fmt.Println(m.Counter.PollCount)
-		fmt.Println(m.Gauge.RandomValue)
-		reportMetrics(m)
-	}
+	pollMetrics(m)
+
+	//	go pollMetrics(m)
+	//	for _ = range time.Tick(pollInterval) {
+	//		fmt.Println(m.Counter.PollCount)
+	//		fmt.Println(m.Gauge.RandomValue)
+	//		reportMetrics(m)
+	//	}
+	//parseMetric(*m)
+	z := reflect.ValueOf(*m)
+	r := new(Report)
+	r.Gauge = make(map[string]float64)
+	r.Counter = make(map[string]int64)
+	parseMetric(&root, &metricName, z, r)
+	//fmt.Printf("%#v", r)
 }
 
 func pollMetrics(m *Metrics) {
-	for {
-		mem := &runtime.MemStats{}
-		runtime.ReadMemStats(mem)
-		incPollCount(&pollCount)
-		randomValue := rand.Float64() * 1000
-		m.Gauge.MemStats = mem
-		m.Gauge.RandomValue = randomValue
-		m.Counter.PollCount = pollCount
-		time.Sleep(pollInterval)
+	//for {
+	mem := &runtime.MemStats{}
+	runtime.ReadMemStats(mem)
+	incPollCount(&pollCount)
+	randomValue := rand.Float64() * 1000
+	m.Gauge.MemStats = mem
+	m.Gauge.RandomValue = randomValue
+	m.Counter.PollCount = pollCount
+	//	time.Sleep(pollInterval)
+	//}
+}
+
+func parseMetric(root, metricName *string, data reflect.Value, report *Report) {
+	types := data.Type()
+	switch types.Name() {
+	case "Gauge":
+		*root = GaugePrefix
+	case "Counter":
+		*root = CounterPrefix
+	}
+
+	switch data.Kind() {
+	case reflect.Struct:
+		for i := 0; i < data.NumField(); i++ {
+			value := data.Field(i)
+			*metricName = types.Field(i).Name
+			parseMetric(root, metricName, reflect.ValueOf(value.Interface()), report)
+		}
+	case reflect.Ptr:
+		f := data.Elem()
+		parseMetric(root, metricName, f, report)
+	default:
+		prefix := path.Join(UploadsPrefix, *root, *metricName)
+		switch *root {
+		case GaugePrefix:
+			if data.Type().ConvertibleTo(float64Type) {
+				v := data.Convert(float64Type)
+				report.Gauge[prefix] = v.Float()
+			}
+		case CounterPrefix:
+			if data.Type().ConvertibleTo(int64Type) {
+				v := data.Convert(int64Type)
+				report.Counter[prefix] = v.Int()
+			}
+		}
 	}
 }
 
-func reportMetrics(m *Metrics) {
-	memValues := reflect.ValueOf(*m.Gauge.MemStats)
-	memTypes := memValues.Type()
-	for index := range memValues.NumField() {
-		fmt.Printf("%v: %v\n", memTypes.Field(index).Name, memValues.Field(index))
+func reportMetrics(report *Report) {
+	for path, value := range report.Gauge {
+		fmt.Printf("%s: %f\n", path, value/1000)
 	}
-	//fmt.Printf("%#v\n", memTypes.Name())
 }
 
 func incPollCount(pollCount *int64) {
